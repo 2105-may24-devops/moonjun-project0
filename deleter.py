bd54e3928649e5e5399de5c6a7454303343fe7c3
Moonjun Chung <moonjchung@gmail.com>
2021-06-06 18:03:41 -0400

    created logging and updated delete_from_list()

--- deleter.py
+++ deleter.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from project_logging import ProjectLogging
 import shutil
 import os
 
@@ -17,17 +18,33 @@ class Deleter():
                     line = line.strip()
                     self.deletion_list.append(line)
             f.close()
+        os.chdir('../')
+        return
 
     def delete_from_list(self):
         flag = input('Are you sure you want to delete? [Y/n]: ')
         if flag == 'Y':
-            for i in self.deletion_list:
-                print(i)
-                if Path.is_dir(Path(i)):
-                    shutil.rmtree(Path(i), ignore_errors=True)
-                elif Path.is_file(Path(i)):
-                    Path.unlink(Path(i))
+            p_log = ProjectLogging()
+            if Path.is_dir(Path.cwd().joinpath('logs')):
+                os.chdir('./logs')
+                p_log.write_log(self.deletion_list)
+                self.delete_loop()
+            else:
+                Path.mkdir(Path.cwd().joinpath('logs'))
+                print(Path.cwd())
+                os.chdir('./logs')
+                p_log.write_log(self.deletion_list)
+                self.delete_loop()
+                
         elif flag == 'n':
             print("No files were deleted.")
         else:
             print('Incorrect input. Try again.')
+        return
+
+    def delete_loop(self):
+        for i in self.deletion_list:
+            if Path.is_dir(Path(i)):
+                shutil.rmtree(Path(i), ignore_errors=True)
+            elif Path.is_file(Path(i)):
+                Path.unlink(Path(i))
