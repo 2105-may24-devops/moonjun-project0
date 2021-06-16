f8ffa17df3db3a88232d47e4daab7c58f274b17d
Moonjun Chung <moonjchung@gmail.com>
2021-06-16 10:56:18 -0400

    Trying something else with shell script.

--- ansible/create_dummies.sh
+++ ansible/create_dummies.sh
@@ -1,22 +1,19 @@
 #! bin/bash
 
-if [ ! -d "~/p0/project0/test" ]; then
+if [! -d "~/p0/project0/test"]; then
     mkdir ~/p0/project0/test
+
+else
+    rm -rf ~/p0/project0/test
 fi
 
 for i in {1..10}; do
-    if [ ! -d "~/p0/project0/test/dummy-directory$i" ]
-        mkdir ~/p0/project0/test/dummy-directory$i
-        for (( j = 1; j <= 5; j++ )); do
-            if [ ! -e "~/p0/project0/test/dummy-directory$i/dummy-file$j" ]
-                touch ~/p0/project0/test/dummy-directory$i/dummy-file$j
-            fi
-        done
-    fi
+    mkdir ~/p0/project0/test/dummy-directory$i
+    for (( j = 1; j <= 5; j++ )); do
+        touch ~/p0/project0/test/dummy-directory$i/dummy-file$j
+    done
 done
 
-if [ ! -d "~/p0/project0/deletion" ]
-    mkdir ~/p0/project0/deletion
-    cp ~/p0/project0/ansible/test_delete.txt ~/p0/project0/deletion
-    mv ~/p0/project0/ansible/test_delete.txt ~/p0/project0/deletion/files-to-delete.txt
-fi
+mkdir ~/p0/project0/deletion
+cp ~/p0/project0/ansible/test_delete.txt ~/p0/project0/deletion
+mv ~/p0/project0/ansible/test_delete.txt ~/p0/project0/deletion/files-to-delete.txt
